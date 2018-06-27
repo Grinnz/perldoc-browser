a1b6d57879a2bd1545c30d00f49ad7cae9643acf
Dan Book <grinnz@grinnz.com>
2018-06-27 15:09:16 -0400

    option to index functions only

--- lib/PerldocBrowser/Command/index.pm
+++ lib/PerldocBrowser/Command/index.pm
@@ -7,6 +7,7 @@ package PerldocBrowser::Command::index;
 use 5.020;
 use Mojo::Base 'Mojolicious::Command';
 use Mojo::File 'path';
+use Mojo::Util 'getopt';
 use Pod::Simple::Search;
 use experimental 'signatures';
 
@@ -14,6 +15,7 @@ has description => 'Index perldocs for search';
 has usage => "Usage: $0 index [all | <version> ...]\n";
 
 sub run ($self, @versions) {
+  getopt \@versions, '--functions' => \my $functions;
   die $self->usage unless @versions;
   if ($versions[0] eq 'all') {
     @versions = @{$self->app->all_perl_versions};
@@ -21,14 +23,19 @@ sub run ($self, @versions) {
   my $db = $self->app->pg->db;
   foreach my $version (@versions) {
     my $inc_dirs = $self->app->inc_dirs($version) // [];
-    my $pod_paths = Pod::Simple::Search->new->inc(0)->survey(@$inc_dirs);
     my $tx = $db->begin;
-    $self->app->clear_pod_index($db, $version);
+    my $pod_paths;
+    if ($functions) {
+      $pod_paths = {perlfunc => Pod::Simple::Search->new->inc(0)->find('perlfunc', @$inc_dirs)};
+    } else {
+      $pod_paths = Pod::Simple::Search->new->inc(0)->survey(@$inc_dirs);
+      $self->app->clear_pod_index($db, $version);
+    }
     $self->app->clear_function_index($db, $version);
     foreach my $pod (keys %$pod_paths) {
       print "Indexing $pod for $version ($pod_paths->{$pod})\n";
       my $src = path($pod_paths->{$pod})->slurp;
-      $self->app->index_pod($db, $version, $pod, $src);
+      $self->app->index_pod($db, $version, $pod, $src) unless $functions;
 
       if ($pod eq 'perlfunc') {
         print "Indexing functions for $version\n";
