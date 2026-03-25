69912966c17f98af052e14115f025f8dbb5f9bde
Dan Book <grinnz@grinnz.com>
2026-03-25 02:03:08 -0400

    select text in search input after focusing

--- public/js/perldoc-focus-search.js
+++ public/js/perldoc-focus-search.js
@@ -2,7 +2,9 @@ document.addEventListener('keydown', function (event) {
   if ((event.key === 's' || event.key === 'S')
       && !event.ctrlKey && !event.altKey && !event.metaKey
       && document.activeElement.tagName.toLowerCase() !== 'input') {
-    document.getElementById('search-input').focus();
+    var searchinput = document.getElementById('search-input');
+    searchinput.focus();
+    searchinput.select();
     event.preventDefault();
   }
 });
